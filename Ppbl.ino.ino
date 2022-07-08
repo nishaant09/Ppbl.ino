6e300351b90a8c78b927af3cfca30ed1015bfca9
nishaant09 <105371885+nishaant09@users.noreply.github.com>
2022-07-08 11:45:59 +0530

    Update Ppbl.ino.ino

--- Ppbl.ino.ino
+++ Ppbl.ino.ino
@@ -8,36 +8,36 @@ String apiKey = "CWMFN3TMX1M8DGMK";
 
  
 // Variables
-int pulsePin = A0; // Pulse Sensor purple wire connected to analog pin 0
-int blinkPin = 7 ; // pin to blink led at each beat
-int fadePin = 13; // pin to do fancy classy fading blink at each beat
+int pulsePin = A0; 
+int blinkPin = 7 ; 
+int fadePin = 13; 
 int fadeRate = 0; // used to fade LED on with PWM on fadePin
  
  
-volatile int BPM; // int that holds raw Analog in 0. updated every 2mS
-volatile int Signal; // holds the incoming raw data
-volatile int IBI = 600; // int that holds the time interval between beats! Must be seeded!
-volatile boolean Pulse = false; // "True" when User's live heartbeat is detected. "False" when nota "live beat".
+volatile int BPM; 
+volatile int Signal; 
+volatile int IBI = 600; 
+volatile boolean Pulse = false; 
 volatile boolean QS = false; // becomes true when Arduoino finds a beat.
  
 static boolean serialVisual = true; 
-volatile int rate[10]; // array to hold last ten IBI values
-volatile unsigned long sampleCounter = 0; // used to determine pulse timing
+volatile int rate[10]; 
+volatile unsigned long sampleCounter = 0; 
 volatile unsigned long lastBeatTime = 0; 
 volatile int P = 512; 
 volatile int T = 512; 
-volatile int thresh = 525; // used to find instant moment of heart beat, seeded
-volatile int amp = 100; // used to hold amplitude of pulse waveform, seeded
+volatile int thresh = 525; 
+volatile int amp = 100; 
 volatile boolean firstBeat = true; 
 volatile boolean secondBeat = false; 
  
 void setup()
 {
 lcd.begin(16, 2);
-pinMode(blinkPin,OUTPUT); // pin that will blink to your heartbeat!
-pinMode(fadePin,OUTPUT); // pin that will fade to your heartbeat!
-Serial.begin(115200); // we agree to talk fast!
-interruptSetup(); // sets up to read Pulse Sensor signal every 2mS
+pinMode(blinkPin,OUTPUT); 
+pinMode(fadePin,OUTPUT); 
+Serial.begin(115200); 
+interruptSetup(); 
 
  
 lcd.clear();
@@ -99,49 +99,47 @@ analogWrite(fadePin,fadeRate); // fade LED
 void interruptSetup()
 {
 
-TCCR2A = 0x02; // DISABLE PWM ON DIGITAL PINS 3 AND 11, AND GO INTO CTC MODE
-TCCR2B = 0x06; // DON'T FORCE COMPARE, 256 PRESCALER
-OCR2A = 0X7C; // SET THE TOP OF THE COUNT TO 124 FOR 500Hz SAMPLE RATE
-TIMSK2 = 0x02; // ENABLE INTERRUPT ON MATCH BETWEEN TIMER2 AND OCR2A
-sei(); // MAKE SURE GLOBAL INTERRUPTS ARE ENABLED
+TCCR2A = 0x02; 
+TCCR2B = 0x06; 
+OCR2A = 0X7C; 
+TIMSK2 = 0x02; 
+sei(); 
 }
 void serialOutput()
 { // Decide How To Output Serial.
 if (serialVisual == true)
 {
-arduinoSerialMonitorVisual('-', Signal); // goes to function that makes Serial Monitor Visualizer
+arduinoSerialMonitorVisual('-', Signal);
 }
 else
 {
-sendDataToSerial('S', Signal); // goes to sendDataToSerial function
+sendDataToSerial('S', Signal); 
 }
 }
 void serialOutputWhenBeatHappens()
 {
-if (serialVisual == true) // Code to Make the Serial Monitor Visualizer Work
+if (serialVisual == true) 
 {
-Serial.print("*** Heart-Beat Happened *** "); //ASCII Art Madness
+Serial.print("*** Heart-Beat Happened *** "); 
 Serial.print("BPM: ");
 Serial.println(BPM);
 }
 else
 {
-sendDataToSerial('B',BPM); // send heart rate with a 'B' prefix
-sendDataToSerial('Q',IBI); // send time between beats with a 'Q' prefix
+sendDataToSerial('B',BPM); 
+sendDataToSerial('Q',IBI); 
 }
 }
 void arduinoSerialMonitorVisual(char symbol, int data )
 {
-const int sensorMin = 0; // sensor minimum, discovered through experiment
+const int sensorMin = 0; 
 const int sensorMax = 1024; // sensor maximum, discovered through experiment
-int sensorReading = data; // map the sensor range to a range of 12 options:
+int sensorReading = data; 
 int range = map(sensorReading, sensorMin, sensorMax, 0, 11);
-// do something different depending on the
-// range value:
 switch (range)
 {
 case 0:
-Serial.println(""); /////ASCII Art Madness
+Serial.println(""); 
 break;
 case 1:
 Serial.println("---");
@@ -184,35 +182,32 @@ void sendDataToSerial(char symbol, int data )
 Serial.print(symbol);
 Serial.println(data);
 }
-ISR(TIMER2_COMPA_vect) //triggered when Timer2 counts to 124
+ISR(TIMER2_COMPA_vect) 
 {
-cli(); // disable interrupts while we do this
-Signal = analogRead(pulsePin); // read the Pulse Sensor
-sampleCounter += 2; // keep track of the time in mS with this variable
-int N = sampleCounter - lastBeatTime; // monitor the time since the last beat to avoid noise
-// find the peak and trough of the pulse wave
+cli(); 
+Signal = analogRead(pulsePin); 
+sampleCounter += 2; 
+int N = sampleCounter - lastBeatTime; 
  
-if(Signal < thresh && N > (IBI/5)*3) // avoid dichrotic noise by waiting 3/5 of last IBI
+if(Signal < thresh && N > (IBI/5)*3) 
 {
-if (Signal < T) // T is the trough
+if (Signal < T) 
 {
-T = Signal; // keep track of lowest point in pulse wave
+T = Signal; 
 }
 }
 if(Signal > thresh && Signal > P)
 { // thresh condition helps avoid noise
 P = Signal; // P is the peak
-} // keep track of highest point in pulse wave
-// NOW IT'S TIME TO LOOK FOR THE HEART BEAT
-// signal surges up in value every time there is a pulse
+} 
 if (N > 250)
 { // avoid high frequency noise
 if ( (Signal > thresh) && (Pulse == false) && (N > (IBI/5)*3) )
 {
-Pulse = true; // set the Pulse flag when we think there is a pulse
-digitalWrite(blinkPin,HIGH); // turn on pin 13 LED
-IBI = sampleCounter - lastBeatTime; // measure time between beats in mS
-lastBeatTime = sampleCounter; // keep track of time for next pulse
+Pulse = true; 
+digitalWrite(blinkPin,HIGH); 
+IBI = sampleCounter - lastBeatTime; 
+lastBeatTime = sampleCounter; 
  
 if(secondBeat)
 { // if this is the second beat, if secondBeat == TRUE
